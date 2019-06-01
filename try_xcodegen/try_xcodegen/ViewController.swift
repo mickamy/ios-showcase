5ad0c0951d7671da16a0440c2ca54f7b4e251671
mickamy <t.mikami1992@gmail.com>
2019-06-01 14:13:27 +0900

    Create project to try XcodeGen

---
+++ try_xcodegen/try_xcodegen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  try_xcodegen
+//
+//  Created by Tetsuro Mikami on 2019/06/01.
+//  Copyright © 2019 Tetsuro Mikami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
