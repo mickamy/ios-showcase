57e38806f3cf615b3b716dd7f6a9b2845e6ad208
mickamy <t.mikami1992@gmail.com>
2020-03-28 17:34:37 +0900

    Create TakeAPhotoViewController

---
+++ AVFoundation/AVFoundation/camera/TakeAPhotoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TakeAPhotoViewController.swift
+//  avfoundation
+//
+//  Created by mickamy on 2020/03/28.
+//  Copyright © 2020 mickamy. All rights reserved.
+//
+
+import UIKit
+
+final class TakeAPhotoViewController: UIViewController {
+    @IBOutlet private weak var cameraOutputView: UIView!
+    @IBOutlet private weak var capturedImageView: UIImageView!
+    @IBOutlet private weak var overlayView: CameraOverlayView!
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
