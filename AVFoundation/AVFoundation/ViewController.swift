152feac9bc8c41e7ad71d09ab3dcb45e68373d28
mickamy <t.mikami1992@gmail.com>
2020-03-28 15:22:30 +0900

    Create project for trying AVFoundation

---
+++ AVFoundation/AVFoundation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  avfoundation
+//
+//  Created by mickamy on 2020/03/28.
+//  Copyright © 2020 mickamy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
