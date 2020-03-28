e5fd2b2a09cfd84e418a2a4b387816d27c8a5cbf
mickamy <t.mikami1992@gmail.com>
2020-03-28 15:45:33 +0900

    Create ViewController UI and use it as a root

--- AVFoundation/AVFoundation/ViewController.swift
+++ AVFoundation/AVFoundation/ViewController.swift
@@ -8,13 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapTakeAPhotoButton(_ sender: Any) {
+        
+    }
 }
-
